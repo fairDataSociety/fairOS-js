b2d6f102cc85e6a3db5e0aa4a81f7dbd1de30587
Igor Shadurin <igor.shadurin@gmail.com>
2021-08-02 15:26:21 +0300

    tested pod open

--- test/1_user.test.js
+++ test/1_user.test.js
@@ -1,5 +1,4 @@
-const {apiNoAuth, fakeUsers, apiAuth} = require('./utils');
-const FairOS = require('../fairos.min');
+const {apiNoAuth, fakeUsers, apiAuth, getRandomPodName} = require('./utils');
 
 jest.setTimeout(60000);
 
@@ -186,4 +185,38 @@ test('User create new pod', async () => {
         expect(data.code).toBe(400);
         expect(data.message).toBe('pod new: pod already exists');
     }
+});
+
+test('User open created pod', async () => {
+    const user = fakeUsers.podUser;
+    const podName = fakeUsers.podUser.podName;
+    const randomPodName = getRandomPodName();
+
+    let data;
+
+    try {
+        await apiNoAuth.podOpen(podName, user.password);
+        expect(true).toBe(false);
+    } catch (e) {
+        const data = e.response.data;
+        expect(data.code).toBe(400);
+        expect(data.message).toBe('cookie: invalid cookie: http: named cookie not present');
+    }
+
+    data = (await apiAuth.userLogin(user.username, user.password)).data;
+    expect(data.code).toBe(200);
+    expect(data.message).toBe('user logged-in successfully');
+
+    try {
+        await apiAuth.podOpen(randomPodName, user.password);
+        expect(true).toBe(false);
+    } catch (e) {
+        const data = e.response.data;
+        expect(data.code).toBe(400);
+        expect(data.message).toBe('pod open: invalid pod name');
+    }
+
+    data = (await apiAuth.podOpen(podName, user.password)).data;
+    expect(data.code).toBe(200);
+    expect(data.message).toBe('pod opened successfully');
 });
--- test/bugs/0_test.test.js
+++ test/bugs/0_test.test.js
@@ -5,25 +5,44 @@ test('User create new pod', async () => {
     // const podName = getRandomPodName();
     const user = fakeUsers.podUser;
     const podName = fakeUsers.podUser.podName;
+    const randomPodName = getRandomPodName();
 
-    await apiNoAuth.userSignup(user.username, user.password);
-
-    let data = (await apiAuth.userLogin(user.username, user.password)).data;
-    expect(data.code).toBe(200);
-    expect(data.message).toBe('user logged-in successfully');
-
-    data = (await apiAuth.podNew(podName, user.password)).data;
-    expect(data.code).toBe(201);
-    expect(data.message).toBe('pod created successfully');
+    let data;
 
     try {
-        await apiAuth.podNew(podName, user.password);
+        await apiNoAuth.podOpen(podName, user.password);
         expect(true).toBe(false);
     } catch (e) {
         const data = e.response.data;
         expect(data.code).toBe(400);
-        expect(data.message).toBe('pod new: pod already exists');
+        expect(data.message).toBe('cookie: invalid cookie: http: named cookie not present');
     }
+
+    data = (await apiAuth.userLogin(user.username, user.password)).data;
+    expect(data.code).toBe(200);
+    expect(data.message).toBe('user logged-in successfully');
+
+    try {
+        await apiAuth.podOpen(randomPodName, user.password);
+        expect(true).toBe(false);
+    } catch (e) {
+        const data = e.response.data;
+        expect(data.code).toBe(400);
+        expect(data.message).toBe('pod open: invalid pod name');
+    }
+
+    data = (await apiAuth.podOpen(podName, user.password)).data;
+    expect(data.code).toBe(200);
+    expect(data.message).toBe('pod opened successfully');
+
+    // try {
+    //     await apiAuth.podNew(podName, user.password);
+    //     expect(true).toBe(false);
+    // } catch (e) {
+    //     const data = e.response.data;
+    //     expect(data.code).toBe(400);
+    //     expect(data.message).toBe('pod new: pod already exists');
+    // }
 
     // console.log('111data', data);
     // expect(data.code).toBe(201);
